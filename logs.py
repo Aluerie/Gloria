0eb2d2c6d93edf468010a7a141f634b13f72059f
Aluerie❤ <aluerie@outlook.com>
2024-10-10 14:44:59 +0300

    Testing Steam.py

---
+++ logs.py
@@ -0,0 +1,66 @@
+from __future__ import annotations
+
+import logging
+import platform
+from contextlib import contextmanager
+from logging.handlers import RotatingFileHandler
+from typing import TYPE_CHECKING, Any
+
+if TYPE_CHECKING:
+    from collections.abc import Generator
+
+__all__ = ("setup_logging",)
+
+# generated at https://patorjk.com/software/taag/ using "Standard" font
+ASCII_STARTING_UP_ART = r"""
+  ____           _             _   _
+ |  _ \ ___  ___| |_ __ _ _ __| |_(_)_ __   __ _
+ | |_) / _ \/ __| __/ _` | '__| __| | '_ \ / _` |
+ |  _ <  __/\__ \ || (_| | |  | |_| | | | | (_| |
+ |_| \_\___||___/\__\__,_|_|   \__|_|_| |_|\__, |
+                                           |___/
+
+            [ RESTARTING ]
+"""
+
+
+@contextmanager
+def setup_logging() -> Generator[Any, Any, Any]:
+    """Setup logging."""
+    log = logging.getLogger()
+    log.setLevel(logging.DEBUG)
+
+    try:
+        # Stream Handler
+        handler = logging.StreamHandler()
+        fmt = logging.Formatter(
+            "{asctime} | {levelname:<7} | {name:<23} | {lineno:<4} | {funcName:<30} | {message}",
+            "%H:%M:%S %d/%m",
+            style="{",
+        )
+
+        handler.setFormatter(fmt)
+        log.addHandler(handler)
+
+        # File Handler
+        file_handler = RotatingFileHandler(
+            filename=".steam.log",
+            encoding="utf-8",
+            mode="w",
+            maxBytes=9 * 1024 * 1024,  # 9 MiB
+            backupCount=5,  # Rotate through 5 files
+        )
+        file_handler.setFormatter(fmt)
+        log.addHandler(file_handler)
+
+        if platform.system() == "Linux":
+            # so start-ups in logs are way more noticeable
+            log.info(ASCII_STARTING_UP_ART)
+
+        yield
+    finally:
+        # __exit__
+        handlers = log.handlers[:]
+        for h in handlers:
+            h.close()
+            log.removeHandler(h)
---
+++ main.py
@@ -0,0 +1,94 @@
+from __future__ import annotations
+
+import asyncio
+import logging
+import sys
+from typing import override
+
+import discord
+import steam
+from discord.ext import commands, tasks
+
+import config
+from logs import setup_logging
+
+if sys.platform == "win32":
+    # wtf why it bugs out without it
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+
+
+class DiscordBot(commands.Bot):
+    def __init__(self) -> None:
+        super().__init__(
+            command_prefix=commands.when_mentioned_or("!"),
+            intents=discord.Intents.default(),
+            description="A simple bot that can get steam user info",
+        )
+        self.steam = steam.Client()  # attach a steam.Client instance to the bot
+
+    async def on_ready(self) -> None:
+        await self.steam.wait_until_ready()
+        print("Ready")
+
+    @override
+    async def start(self, token: str, username: str, password: str) -> None:
+        await asyncio.gather(
+            super().start(token),
+            self.steam.login(username, password),
+        )  # start the client and bot concurrently
+
+    @override
+    async def close(self) -> None:
+        await self.steam.close()  # make sure to close the client when we close the discord bot
+        await super().close()
+
+    @tasks.loop(minutes=10)
+    async def check_rp(self) -> None:
+        log.debug("Checking Rich Presence")
+        user = self.steam.get_user(config.IRENE_ID64)
+        if user:
+            print(user.rich_presence)
+
+        if self.check_rp.current_loop == 2:
+            await self.tree.sync()
+
+    @check_rp.before_loop
+    async def check_rp_before_loop(self) -> None:
+        await self.wait_until_ready()
+        await self.steam.wait_until_ready()
+
+
+class UserNotFound(commands.BadArgument):
+    """For when a matching user cannot be found"""
+
+    def __init__(self, argument: str) -> None:
+        self.argument = argument
+        super().__init__(f"User {argument!r} not found.")
+
+
+bot = DiscordBot()
+
+
+@bot.tree.command()
+async def user(interaction: discord.Interaction[DiscordBot]) -> None:
+    """Show some basic info on a steam user"""
+    user = await interaction.client.steam.fetch_user(config.IRENE_ID64)
+    embed = discord.Embed(description=user.name)
+    embed.set_thumbnail(url=user.avatar.url)
+    embed.add_field(name="64 bit ID:", value=str(user.id64))
+    embed.add_field(name="Currently playing:", value=f"{user.app or 'Nothing'}")
+    embed.add_field(name="Friends:", value=len(await user.friends()))
+    embed.add_field(name="Apps:", value=len(await user.apps()))
+    await interaction.response.send_message(f"Info on {user.name}", embed=embed)
+
+
+async def main() -> None:
+    async with bot:
+        await bot.start(config.DISCORD_TOKEN, config.STEAM_USERNAME, config.STEAM_PASSWORD)
+
+
+with setup_logging():
+    asyncio.run(main())
